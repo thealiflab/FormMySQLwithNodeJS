6032bccf039cc2415b93a35c0f18d43e5f6bc5f8
Alif <mail@ahmedalif.com>
2020-05-06 13:21:16 +0600

    database connected successfully

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var mysql = require("mysql");
 
 const app = express();
 
@@ -14,12 +15,30 @@ app.listen(3000, function(){
     console.log("Server started....");
 });
 
+// MySQL setup
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    pass: '',
+    database: 'formmysqlwithnodejs'
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+
+    console.log('Database is connect successfully...');
+});
+
+connection.end();
+
+
 app.post("/",function(req,res){
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var username = req.body.username;
     var email = req.body.email;
     var phone = req.body.phone;
+    var gender = req.body.gender;
 
-    console.log(firstName,lastName,username,email,phone);
+    console.log(firstName,lastName,username,email,phone,gender);
 });
